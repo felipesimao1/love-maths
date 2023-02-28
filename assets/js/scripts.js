6d42c4862097f9bd26f270109a5d8d447726428d
Felipe Simao <31260554+felipesimao1@users.noreply.github.com>
2023-02-28 20:48:00 +0000

    adding js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,24 +10,47 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
+
 });
 
-function runGame() {
+function runGame(gameType) {
     // Create random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25 ) +1;
     let num2 = Math.floor(Math.random() * 25 ) +1;
+
+    if(gameType === "addition"){
+        displayAdditionQuestion(num1,num2);
+    }else{
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`
+    }
 }
 
 function checkAnswer() {
 
 }
 
+/**
+ *  Gets the operands (numbers) and the operators (plus, minus, etc)
+ * directly from the dm, and returns the correct answer
+ */
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerHTML);
+    let operand2 = parseInt(document.getElementById('operand2').innerHTML);
+    let operator = document.getElementById("operator").innerHTML;
 
+    if(operator === "+"){
+        return [operand1 + operand2, "addition"];
+    }else{
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 }
 
 function incrementScore() {
@@ -38,7 +61,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent =  "+";
 
 }
 
