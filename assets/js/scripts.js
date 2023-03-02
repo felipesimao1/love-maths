154aa4c33848c90ed5f13bd2791b57ef435dcb9c
Felipe Simao <31260554+felipesimao1@users.noreply.github.com>
2023-03-02 20:22:27 +0000

    finalizend add count

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -43,9 +43,11 @@ function checkAnswer() {
     let isCorrect = userAnswer === calculatedAnswer[0];
 
     if(isCorrect){
-        alert("Hey! You got it rigt! :D")
+        alert("Hey! You got it rigt! :D");
+        incrementScore();
     }else{
-        alert(`Awwww... you answered ${userAnswer}. The correct amswer was ${calculatedAnswer[0]}`);
+        alert(`Awwww... you answered ${userAnswer}. The correct amswer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -68,12 +70,20 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Gets the current tally or incorrect from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
